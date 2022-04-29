e1b2aee5b8745dc4d26a753afd20920ae07e89d1
Justin Hamerly <suvimountain@icloud.com>
2022-04-28 18:53:09 -0700

    added counter

--- game_logic.js
+++ game_logic.js
@@ -4,9 +4,23 @@ function rollDice(num=6){
     let number = Math.ceil(Math.random() * 6)
     diceArray.push(number)
   }
+  diceArray.sort();
   return diceArray;
 }
 
 function calculateScore(dice){
-  if(dice.length) > 6 
+  if(dice.length > 6){
+    console.log('You Cheater!')
+  }
+}
+
+function counter(dice){
+  const counts = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+  dice.forEach(value => {
+    counts[value] ++
+  })
+  const countsAsArray = Object.entries(counts);
+  const filtered = countsAsArray.filter(count =>  count[1] > 0);
+  console.log(filtered)
+  return filtered;
 }

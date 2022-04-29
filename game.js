99379f4ae0ec5559d0e9ca2fb11d2f961b4a831b
Justin Hamerly <suvimountain@icloud.com>
2022-04-28 20:04:30 -0700

    game class created

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
 import { rollDice, calculateScore } from './game_logic';
+import Banker from './banker';
+
+class Game{
+  constructor(numRounds = 20){
+    this.banker = new Banker();
+    this.numRounds = numRounds;
+    this.roundNum = 0;
+  }
+
+
+}
